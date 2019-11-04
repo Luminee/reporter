4d12ef3103dc7d46f32498af680e4d984f9fe52a
LuminEe <1123462990@qq.com>
2019-11-04 14:26:47 +0800

    append config

---
+++ config/reporter.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /**
+     * Relation Account Model
+     */
+    'account' => [
+
+        'model' => 'App\Models\User\Account',
+
+        'name' => 'nickname'
+    ],
+
+];
--- src/Models/Log.php
+++ src/Models/Log.php
@@ -8,6 +8,8 @@ class Log extends BaseModel
 {
     protected $table = 'reporter_log';
 
+    protected $account = [];
+
     protected $fillable = ['scope_id', 'account_id', 'action_id', 'object_type', 'object_id', 'content'];
 
     public function scope()
@@ -15,8 +17,22 @@ class Log extends BaseModel
         return $this->belongsTo('Luminee\Reporter\Models\Scope', 'scope_id', 'id');
     }
 
+    public function account()
+    {
+        return $this->belongsTo($this->getAccount()['model'], 'account_id', 'id');
+    }
+
     public function action()
     {
         return $this->belongsTo('Luminee\Reporter\Models\Action', 'action_id', 'id');
     }
+
+    protected function getAccount()
+    {
+        if (empty($this->account)) {
+            $this->account = config('reporter.account');
+        }
+        return $this->account;
+    }
+
 }
